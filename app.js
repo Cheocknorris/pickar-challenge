b1ea77aa8bff013b559a568ec5a999d583b81ef3
Eliseo Quintanilla <eliseo.quintanillagz@gmail.com>
2020-06-28 17:09:33 +0200

    using EJS form templating

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.static("./images"));
+app.use(express.static("./public"));
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-    res.send('hello');
+    res.render('converter', {foo: '5'});
 });
 
 app.listen(3000, function() {
